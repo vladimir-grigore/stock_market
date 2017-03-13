bb95302f9cbf9561eb66e3ad27256d3243be1b31
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 00:55:48 +0000

    Add comments

--- maxProfit.js
+++ maxProfit.js
@@ -1,55 +1,31 @@
-/*
-Goal
-Practice solving word problems by developing looping and calculation
-algorithms that work with data in an array format.
-
-Exercise
-Create a function maxProfit, which, given a list of stock
-prices for a given day, returns the maximum profit that could
-have been made by buying a stock at the given price and then
-selling the stock later on. For example if
-the input is: [45, 24, 35, 31, 40, 38, 11] then your program
-should return 16 because if you bought the stock
-at $24 and sold it at $40, a profit of $16 was made and
-this is the largest profit that could be made. If no profit could have been made, return -1.
-
-Strategy 1: Brute Force
-If you think about it for a few minutes, you'll realize
-this problem is non-trivial to solve. So, let's not worry
-about a "clean" or "efficient" solution. Here's the
-recommended "brute force" strategy: for each pair of
-valid dates, calculate the profit, then, figure out
-which dates corresponded to the max profit.
-
-For example, if your values were: [a, b, c], you would check profits for: [a,b], [a,c] and [b,c].
-
-Strategy 2: Elegant Solution
-Try to come up with a non-brute-force solution (
-warning: not easy). You may want to read up on: Divide and Conquer.
-*/
-
-
 function maxProfit(arr){
   //store all profit possibilities in an array
   var profits = [];
 
-  var compareNum = function(index, arr){
+  var getAllProfits = function(index, arr){
+    //Take the number at the specified position and compare
+    //it with all the following numbers
     var num = arr[index];
     var max_profit = 0;
 
-    //start from the requested index and get profits
+    //Start from the requested index and get profits
     for (index; index < arr.length; index++){
       if(num < arr[index]){
+        //Store a list of profits in an array
         profits.push(arr[index] - num);
       }
     }
   }
 
+  //Loop through the array and get all possible profits for each number
   for (var i = 0; i < arr.length; i++){
-    compareNum(i, arr);
+    //Get all possible profits starting from a specific index
+    getAllProfits(i, arr);
   }
 
+  //Return the biggest number in the profits array
   return Math.max.apply(null, profits);
 }
 
+//Export to be used in unit testing
 module.exports = maxProfit;
